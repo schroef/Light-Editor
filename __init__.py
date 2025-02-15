5bd1ef9a89ce2ff78a8c5e540e27ac0f3a4edd11
riouxr <riouxr@gmail.com>
2025-02-14 22:52:11 -0500

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Light Editor",
     "author": "Robert Rioux aka Blender Bob, Rombout Versluijs",
     "location": "3Dview > Light Editor",
-    "version": (1, 8, 0),
+    "version": (1, 8, 1),
     "blender": (4, 2, 0),
     "description": "A Light Editor and Light Linking addon",
     "category": "Object",
